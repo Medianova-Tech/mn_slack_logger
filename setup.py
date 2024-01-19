5896cb208149df34997b352868c234232da5a30c
Ibrahim Uluocak <ibrahimuluocak@Ibrahims-MacBook-Air.local>
2024-01-19 17:19:07 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mn_slack_logger',
+    packages=['mn_slack_logger'],
+    description='Slack logger by Medianova',
+    version='0.1',
+    author='Medianova',
+    keywords=['python', 'slack logger', 'medianova'],
+    install_requires=['requests==2.31.0']
+    )
